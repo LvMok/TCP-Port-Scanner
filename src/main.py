205ef5de4cd6f87e170e4a15bf616b7d289b2085
Wonea1 <158188942+LvMok@users.noreply.github.com>
2025-03-10 17:52:43 +0900

    temp 6

--- src/main.py
+++ src/main.py
@@ -11,15 +11,49 @@ class Window(QMainWindow):
         super().__init__()
         self.ui = uic.loadUi("Scanner_ui.ui",self)
         self.ui.show()
+
+        RangeFrame = self.Setting_Tab.findChild(QWidget,'TargetPort').findChild(QWidget,'RangeMode')
+        FileFrame = self.Setting_Tab.findChild(QWidget,'TargetPort').findChild(QWidget,'FileMode')
         
-        self.run()
+        RangeFrame.findChild(QWidget,'RangeConfirm').clicked.connect(self.Range_input)
+        FileFrame.findChild(QWidget,'FileConfirm').clicked.connect(self.File_input)
+        self.ChangeFile.clicked.connect(self.Change_File)
+        self.ChangeRange.clicked.connect(self.Change_range)
+
+        self.Scan.clicked.connect(self.scan)
+        
     
-    def run(self):
-        pass
+    def scan(self):
+        # get Setting Part
+        target = self.Target.toPlainText()
+        if self.ChangeRange.isChecked():
+            print("debug")
+    
+    def Range_input(self):
+        print("Trigg Range")
+    
+    def File_input(self):
+        print("Trigg File")
+
+    def Change_File(self):
+        self.FileMode.show()
+        self.RangeMode.hide()
+    
+    def Change_range(self):
+        self.FileMode.hide()
+        self.RangeMode.show()
+
+def suppress_qt_warnings():
+    os.environ["QT_DEVICE_PIXEL_RATIO"] = "0"
+    os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+    os.environ["QT_SCREEN_SCALE_FACTORS"] = "1"
+    os.environ["QT_SCALE_FACTOR"] = "1"
 
 if __name__ == '__main__':
     os.chdir(str(os.getcwd())+'\\src')
     
+    suppress_qt_warnings()
+
     app = QApplication(sys.argv)
     myWindow = Window()
     myWindow.show()
